a082e2c0064262cd10544cb30744fcc7a50ef6b5
Sarunas Iljeitis <sarunas.iljeitis@gmail.com>
2020-06-10 16:33:34 +0300

    flask official tutorial up to "templates"

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,25 @@
-from app import app
+"""Route declaration."""
+from flask import current_app as app
+from flask import render_template, url_for, redirect
+
+from .forms import ContactForm
+
 
 @app.route('/')
-@app.route('/index')
+def home():
+    """Landing page."""
+    nav = [{'name': 'Home', 'url': 'https://example.com/1'},
+           {'name': 'About', 'url': 'https://example.com/2'},
+           {'name': 'Pics', 'url': 'https://example.com/3'}]
+    return render_template('home.html',
+                           nav=nav,
+                           title="Jinja Demo Site",
+                           description="Smarter page templates \
+                                with Flask & Jinja.")
 
-def index():
-    return "Hello, World"
+@app.route('/contact', methods=('GET', 'POST'))
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        return redirect(url_for('success'))
+    return render_template('index.html', form=form)
